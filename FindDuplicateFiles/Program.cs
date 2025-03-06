36f37846544cbdba9bf882ac8c6dad66f95e6ec5
Ramiro Andrés Bedoya E. <iamramiroo@gmail.com>
2025-03-06 14:48:54 -0500

    Refactor files

--- FindDuplicateFiles/Program.cs
+++ FindDuplicateFiles/Program.cs
@@ -1,8 +1,8 @@
 ﻿
 using System.Linq;
 
-string rootPath = @"C:\Users\ramir\OneDrive\Album de Fotografias\";
-string folderAllowedToDelete = @"\Whatsapp\";
+string rootPath = @"C:\Users\ramir\OneDrive\";
+string folderAllowedToDelete = @"\AudioLibros\ingles_class\";
 var canDeleteFile = false;
 
 //Capture parameter Delete=true from command line for allow delete
@@ -11,58 +11,10 @@ if (args.Length > 0)
     canDeleteFile = args[0].Contains("Delete=true");
 }
 
-string[] files = Directory.GetFiles(rootPath, "*.*", SearchOption.AllDirectories);
 
-var groups = files.GroupBy(f => new FileInfo(f).Length);
-foreach (var group in groups.OrderBy(i => i.First().Length))
-{
-    if (group.Count() <= 1)
-    {
-        continue;
-    }
-    
-    foreach (var filesByName in group.GroupBy(f => new FileInfo(f).Name))
-    {
-        if (filesByName.Count() <= 1)
-        {
-            continue;
-        }
-        Console.WriteLine("Duplicate file size: {0}", ConvertBytesToHumanRedable(group.Key));
-        Console.WriteLine($"\t{filesByName.Key} - {filesByName.Count()} files");
-        var count = 0;
-        foreach (var file in filesByName)
-        {
+RemoveAllFiles(rootPath, folderAllowedToDelete, canDeleteFile);
 
-            if (file.Contains(folderAllowedToDelete))
-            {
-                Console.WriteLine($"\t{file} - Allowed to delete {canDeleteFile}");
-
-                if (canDeleteFile && count == 0)
-                {
-                    File.Delete(file);
-                    count++;
-                }
-                continue;
-
-            }
-            Console.WriteLine($"\t{file}");
-
-        }
-
-
-
-    }
-}
-
-//Delete empty folders of rootPath
-foreach (var directory in Directory.GetDirectories(rootPath, "*", SearchOption.AllDirectories))
-{
-    if (Directory.GetFiles(directory).Length == 0 && Directory.GetDirectories(directory).Length == 0)
-    {
-        Console.WriteLine($"Deleting empty folder: {directory}");
-        Directory.Delete(directory, false);
-    }
-}
+RemoveAllEmptyFolders(rootPath);
 
 string ConvertBytesToHumanRedable(long fileLength)
 {
@@ -80,3 +32,71 @@ string ConvertBytesToHumanRedable(long fileLength)
 
 Console.WriteLine(
     "Press any key to continue...");
+
+void RemoveAllFiles(string rootPath, string folderAllowedToDelete, bool canDeleteFile)
+{
+    string[] files = Directory.GetFiles(rootPath, "*.*", SearchOption.AllDirectories);
+
+    var groups = files.GroupBy(f => new FileInfo(f).Length);
+    foreach (var group in groups.OrderBy(i => i.First().Length))
+    {
+        if (group.Count() <= 1)
+        {
+            continue;
+        }
+
+        foreach (var filesByName in group.GroupBy(f => new FileInfo(f).Name))
+        {
+            //filter files with size less than 50MB
+
+            var MAX_FILE = 50000000;
+            if (filesByName.Count() <= 1)
+            {
+                continue;
+            }
+            if (group.Key < MAX_FILE)
+            {
+                continue;
+            }
+
+            Console.WriteLine("Duplicate file size: {0}", ConvertBytesToHumanRedable(group.Key));
+
+            Console.WriteLine($"\t{filesByName.Key} - {filesByName.Count()} files");
+            var count = 0;
+            foreach (var file in filesByName)
+            {
+
+                if (file.Contains(folderAllowedToDelete))
+                {
+                    Console.WriteLine($"\t{file} - Allowed to delete {canDeleteFile}");
+
+                    if (canDeleteFile && count == 0)
+                    {
+                        File.Delete(file);
+                        count++;
+                    }
+                    continue;
+
+                }
+                Console.WriteLine($"\t{file}");
+
+            }
+
+
+
+        }
+    }
+}
+
+static void RemoveAllEmptyFolders(string rootPath)
+{
+    //Delete empty folders of rootPath
+    foreach (var directory in Directory.GetDirectories(rootPath, "*", SearchOption.AllDirectories))
+    {
+        if (Directory.GetFiles(directory).Length == 0 && Directory.GetDirectories(directory).Length == 0)
+        {
+            Console.WriteLine($"Deleting empty folder: {directory}");
+            Directory.Delete(directory, false);
+        }
+    }
+}
